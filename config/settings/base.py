01776a21bf2ce2829daad24fb2ebad48f1b59d4a
ChrisjanWust <chrisjanwust@gmail.com>
2022-06-22 22:11:31 +0200

    Fixes to configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,7 +3,10 @@ Base settings to build other settings files upon.
 """
 from pathlib import Path
 
+import dotenv
 import environ
+
+dotenv.load_dotenv()
 
 ROOT_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # whatshappening/
@@ -23,7 +26,7 @@ DEBUG = env.bool("DJANGO_DEBUG", False)
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # though not all of them may be available with every OS.
 # In Windows, this must be set to your system time zone.
-TIME_ZONE = "GMT+2"
+TIME_ZONE = "Africa/Johannesburg"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
